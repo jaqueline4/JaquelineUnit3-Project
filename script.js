5d3b9e59e67eb80fd1ef15bf8886936043fde891
jaqueline4 <125286170+jaqueline4@users.noreply.github.com>
2023-02-13 09:06:25 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let tittle=document.querySelector("life events");
+let storyopening=document.querySelector("story-opening");
+let buttons=document.querySelector("buttons");
+let optionone =document.querySelector(".option-one");     let optiontwo =document.querySelector(".option-two");     let optiononescreen=document.querySelector("option-one-screen");
+let optiontwoscreen=document.querySelector("option-two-screen");
+let optiononeend=document.querySelector("option-one-end");
+let optiontwoend=document.querySelector("option-two-end");
+
+
+
+
+
+
+
+
+
+
+
